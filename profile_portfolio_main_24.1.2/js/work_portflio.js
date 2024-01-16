f31e9fdeec4b793ab229601cbb26c5ccd854ccd3
hyeyoung31 <154103645+hyeyoung31@users.noreply.github.com>
2024-01-16 23:26:58 +0900

    first commit
    JavaScript version of workportfolio page ( previous version was written in jquery)

--- profile_portfolio_main_24.1.2/js/work_portflio.js
+++ profile_portfolio_main_24.1.2/js/work_portflio.js
@@ -1,20 +1,19 @@
-$(function(){
+const tab = document.querySelectorAll('.tab_menu li');
+const panel = document.querySelectorAll('.panel >li');
+console.log(tab);
+console.log(panel);
+function showContent(num){
+  panel.forEach(function(item){
+    item.style.display="none";
+  });
+  panel[num].style.display="block";
+}
+ showContent(0);
 
-  $(function(){
-    $("ul.panel> li:not("+$("ul.tab li a.selected").attr("href")+")").hide();
- 
-    $('ul.tab li a').click(function(){
-      $('ul.tab li a').removeClass('selected');
-      $(this).addClass('selected');
-      $("ul.panel>li").hide();
-       $($(this).attr("href")).show();
-     return false;
-    });
-  
-
-    });
-  
-
-
-
-});
+ //메뉴클릭 이벤트
+ tab.forEach(function(item,idx){
+  item.addEventListener('click',function(e){
+    e.preventDefault();
+    showContent(idx);
+  });
+ });
