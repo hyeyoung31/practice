619dfe77c08d38b181e26d976b2d22e954c0920a
hyeyoung31 <154103645+hyeyoung31@users.noreply.github.com>
2024-01-29 22:39:43 +0900

    second commit
    Error correction.

--- profile_portfolio_main_24.1.2/js/menu.js
+++ profile_portfolio_main_24.1.2/js/menu.js
@@ -5,23 +5,23 @@ $(function () {
       $(".cross").stop().show();
     });
 
-
-
-    $(".menu-list >li").click(function () {
+    
+    $(".menu-list #nav3").click(function () {
       if ($(this).next().is(":visible")) {
         $(this).next().stop().slideUp(500);
-        $(this).removeClass("active-tab");
         $(this).find("a").removeClass("active");
 
       } else {
-        $(".menu-submenu").stop().slideUp(500);
         $(this).next().stop().slideDown(500);
-        $(".menu-list >li").removeClass("active-tab");
-        $(this).addClass("active-tab");
-        $(".menu-list >li>a").removeClass("active");
         $(this).find("a").addClass("active");
       }
     });
+
+    $(" ccordion-toggle a:before, .accordion-toggle a:after ").click(function () {
+      return false;
+    });
+
+
 
 
     $(".cross").click(function () {
