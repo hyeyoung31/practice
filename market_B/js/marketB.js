d5504f28dbdf61a9e0a885bdf978aa2892f4dc90
hyeyoung31 <154103645+hyeyoung31@users.noreply.github.com>
2024-02-25 23:45:00 +0900

    third commit
    adjusted by jquery

--- market_B/js/marketB.js
+++ market_B/js/marketB.js
@@ -1,24 +1,52 @@
-var targetLink = document.querySelectorAll('.tab_menu li');
-var tabContent = document.querySelectorAll('.panel >li');
+$(function () {
 
-for(var i=0; i<targetLink.length; i++){
+    const tabLink = $('.tabs li a'); // 메인 탭 앵커 태그
+    const nestedLink = $('.nested_tabs li a'); // 서브 탭 앵커 태그
+    // const tabNavLi = $('.tab_nav li'); // 메인 탭 리스트
+    
 
-targetLink[i].addEventListener('click', function(e){
-   e.preventDefault();
-   var orgTarget = e.target.getAttribute('href');
-   console.log(orgTarget);
-   var tabTarget = orgTarget.replace('#','');
+     
+    // 메인 탭 클릭 이벤트 핸들러
+    tabLink.on("click", function (e) {
+      e.preventDefault();
+      const target = $(this).attr('href');
+  
+      // 해당 메인 탭 활성화
+  
+      tabLink.removeClass('active');
+      $(this).addClass('active');
+      $(target).addClass('active').siblings('.active').removeClass('active');
 
-   for(var x =0; x < tabContent.length; x++){
-      tabContent[x].style.display ='none'
-   }
- document.getElementById(tabTarget).style.display = 'block';
+      
+      //네스티드 탭 유지 또는 첫 번째 네스티드 탭 활성화
+      const nestedLinkActive = $(target).find('.nested_tabs li a.active');
 
- for(var j = 0; j<targetLink.length; j++){
-       targetLink[j].classList.remove('selected');
-       e.target.classList.add('selected');
- }
-});   
-}
-document.getElementById('tab1').style.display = 'block'
+      if (!nestedLinkActive.length) {
+          $(target).find('.nested_tabs li:first-child a').trigger('click');
+      } else {
+        nestedLinkActive.trigger('click');
+      }
+  });
+    
+   
+     // 네스티드 탭 클릭 이벤트 핸들러
+     nestedLink.on("click", function (e) {
+      e.preventDefault();
+      const target = $(this).attr("href");
+  
+      // 해당  네스티드 링크 활성화
+      nestedLink.removeClass('active');
+      $(this).addClass('active');
+      $(target).addClass('active').siblings('.active').removeClass("active");
 
+  });
+  
+    // 페이지 로드 시 첫 번째 탭 활성화
+    const tabLinkActive = tabLink.filter('.active');
+    if (!tabLinkActive.length) {
+      tabLink.first().trigger('click');
+    } else {
+      tabLinkActive.trigger('click');
+    }
+
+});
